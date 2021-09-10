c8d9b033db6e58369fdcfab7f24f2870c3b890b5
devferx <ferkki@hotmail.com>
2021-09-10 19:05:31 -0400

    :lipstick: [ADD] Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   purge: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#212529",
+        subtitles: "#425466",
+        bg: "#0e0c20",
+        secondarybuttons: "#0a2540",
+        white: "#ffffff",
+        purple: "#635cff",
+        grey: "#f6f9fc",
+        blue: "#32a7ec",
+        "dark-blue": "#092540",
+        grey: "#b6b6bc",
+        green: "#8bde61",
+        pink: "#d424e6",
+        red: "#f71058",
+        orange: "#e1585f",
+      },
+    },
   },
   variants: {
     extend: {},
